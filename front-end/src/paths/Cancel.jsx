da61f9245d47fc086554f204b966e353c766d401
Tahsin566 <nazmulhassantahsin566@gmail.com>
2024-08-24 20:09:48 +0600

    Changes made

--- front-end/src/paths/Cancel.jsx
+++ front-end/src/paths/Cancel.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Cancel = () => {
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    fetch('https://shopmart-app-backend.onrender.com/dashboard', { credentials: 'include' }).then(res => res.json()).then(data => {
+      if (!data.found) {
+        navigate('/')
+      }
+    })
+  }, [])
+
   return (
-    <div>Cancel</div>
+    <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',minHeight:'100vh'}}>
+      <h2>Payment canceled</h2>
+      <button onClick={
+        ()=>{navigate('/home')}
+      } style={{width:'180px',padding:'5px',backgroundColor:'indigo',color:'white',borderRadius:'5px'}}>Home</button>
+      </div>
   )
 }
 
--- front-end/src/paths/Success.jsx
+++ front-end/src/paths/Success.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { Cartcontext } from '../context/cart'
+import { useNavigate } from 'react-router-dom'
 
 const Success = () => {
+
+  const cart = useContext(Cartcontext)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    fetch('https://shopmart-app-backend.onrender.com/dashboard', { credentials: 'include' }).then(res => res.json()).then(data => {
+      if (!data.found) {
+        navigate('/')
+      }
+    })
+  }, [])
+
   return (
-    <div>Success</div>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <h1>Payment Successful</h1>
+      <p>Amount paid : {cart.gettotalprice()}</p>
+      <button onClick={
+        () => { navigate('/home') }
+      } style={{ width: '180px', padding: '5px', backgroundColor: 'indigo', color: 'white', borderRadius: '5px' }}>Home</button>
+    </div>
   )
 }
 
